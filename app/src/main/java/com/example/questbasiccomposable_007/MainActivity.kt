217c81c53084fc7a7a89e7a4e86a2db73c36126c
Rafi Jadika <Rafi Jadika@LAPTOP-M1GUN5HA>
2024-10-18 21:31:00 +0700

    membuat tulisan indonesia

---
+++ app/src/main/java/com/example/questbasiccomposable_007/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.example.pam3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.questbasiccomposable_007.R
+import com.example.questbasiccomposable_007.ui.theme.QuestBasicComposable_007Theme
+import androidx.compose.material3.Scaffold as Scaffold1
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            QuestBasicComposable_007Theme {
+                Scaffold1(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    BasicComposable(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BasicComposable(modifier: Modifier = Modifier) {
+    Column (verticalArrangement = Arrangement.Top,
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(text = "indonesia",
+            modifier = Modifier,
+            fontSize = 70.sp,
+            color = Color.Red,
+            fontWeight = FontWeight.Bold
+        )
+        
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    QuestBasicComposable_007Theme  {
+        Greeting("Android")
+    }
+}
